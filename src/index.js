4e8fff1c9a20402891abacc40b0f264b1dfa3d52
Grace Kostanich <fl5812597@gmail.com>
2022-07-05 11:37:07 -0700

    Remove blank.js | Add imports for data and getRandomInt | FFix all errors
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import data from "./assets/jokes.json"
-//import ClassName from './Blank.js';
+import data from "./assets/jokes.json";
+import getRandomInt from "./getRandomInt.js";
 
-$(document).ready(function(event) {
-    event.preventDefault();
+
+
+
+$(document).ready(function() {
+  let test = getRandomInt();
+  console.log(data.results[test].Joke);
 });
 
 
