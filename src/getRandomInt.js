c94fd15dd8f56ada9c9601aea888fa8b46c69359
Grace Kostanich <fl5812597@gmail.com>
2022-07-05 10:26:25 -0700

    Add random number generator
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

---
+++ src/getRandomInt.js
@@ -0,0 +1,13 @@
+
+export function getRandomInt() {
+    min = Math.ceil(0);
+    max = Math.floor(29,780);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let index = getRandomInt();
+
+let output = data.results[index].Joke;
+
+`${output}`
+
