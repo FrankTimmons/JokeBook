680bee153d1ac4f916670e2f6738969698a8b3fb
Grace Kostanich <fl5812597@gmail.com>
2022-07-05 10:27:31 -0700

    Add data import from jokes.json
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import data from "./assets/jokes.json"
 //import ClassName from './Blank.js';
 
 $(document).ready(function(event) {
