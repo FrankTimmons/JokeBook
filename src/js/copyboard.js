030c03bdc49f26af1159c6297e7d053a2d2e667b
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-06 15:44:40 -0700

    Expanded on funcationality
    Co-authored-by: Grace Kostanich <fl5812597@gmail.com>
    Co-authored-by: Frank Timmons <franktimmonspdx@gmail.com>
    Co-authored-by: Maxwell Alvord <maxwellalvord@gmail.com>

---
+++ src/js/copyboard.js
@@ -0,0 +1,9 @@
+export default function CopyToClipboard(output)
+  {
+  var r = document.createRange();
+  r.selectNode(document.getElementById(output));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  }
